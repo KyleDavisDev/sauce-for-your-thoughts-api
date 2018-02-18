ee1c9a464ab5f29401b2ce551fd82cfc6ed5d251
Kyle Bonar <kylewb2011@gmail.com>
2018-02-18 15:23:52 -0600

    Passing only relevant information to Model, returns sauce slug, added basic eerror handling.

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,7 +3,13 @@ const Review = mongoose.model("Review");
 
 exports.addReview = async (req, res) => {
   try {
-    const review = new Review(req.body).save();
+    const record = {
+      author: req.body._id,
+      sauce: req.body.sauceID,
+      text: req.body.review || "",
+      rating: req.body.rating
+    };
+    const review = new Review(record).save();
 
     if (!review) {
       const data = {
@@ -16,8 +22,16 @@ exports.addReview = async (req, res) => {
     const data = {
       isGood: true,
       msg: "Successfully added sauce.",
-      sauce: { slug }
+      sauce: { slug: req.body.slug }
     };
     return res.status(200).send(data);
-  } catch (err) {}
+  } catch (err) {
+    //TODO: Better error handling/loggin
+
+    const data = {
+      isGood: false,
+      msg: "Could not add sauce. Make sure all fields are filled and try again."
+    };
+    return res.status(400).send(data);
+  }
 };
