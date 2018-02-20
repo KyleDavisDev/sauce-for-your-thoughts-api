9fa15995744826149d1a3edf8f6e8b26b737a07e
Kyle Bonar <kylewb2011@gmail.com>
2018-02-20 00:06:40 -0600

    Init commit of find reivew by user ID.

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -36,3 +36,38 @@ exports.addReview = async (req, res) => {
     return res.status(400).send(data);
   }
 };
+
+exports.findReviewByUserID = async (req, res) => {
+  try {
+    const query = {
+      author: req.body.user._id,
+      sauce: req.body.sauce._id
+    };
+    const review = await Review.findOne(query, { _id: 1, rating: 1 });
+    if (!review) {
+      const data = {
+        isGood: false,
+        msg: "Could not find sauce."
+      };
+      return res.status(400).send(data);
+    }
+
+    //TODO: figure out how to attach value to local
+    res.locals.sauce.rating = review.rating;
+
+    const data = {
+      isGood: true,
+      msg: "Successfully found sauce.",
+      data: res.locals
+    };
+    return res.status(200).send(data);
+  } catch (err) {
+    //TODO: Better error handling/loggin
+
+    const data = {
+      isGood: false,
+      msg: "Could not add sauce. Make sure all fields are filled and try again."
+    };
+    return res.status(400).send(data);
+  }
+};
