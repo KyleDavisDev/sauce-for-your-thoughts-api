c0f41e2b840837c688e2af042170ec98f4ecb15c
Kyle Bonar <kylewb2011@gmail.com>
2018-03-25 17:56:03 -0500

    Added comments, attach review to response object, return response.

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 const Review = mongoose.model("Review");
 
+/** @description Add review to DB
+ *  @param {Object} review - review to be saved
+ *  @return {Object} attaches review to req.response
+ */
 exports.addReview = async (req, res) => {
   try {
+    //construct review to save
     const record = {
       author: req.body.user._id,
       sauce: req.body.sauce._id,
@@ -10,8 +15,11 @@ exports.addReview = async (req, res) => {
       rating: req.body.review.rating
     };
 
-    const review = await new Review(record).save();
+    //save into DB
+    //TODO limit returned object information
+    const review = await new Review(record).save("text");
 
+    //make sure record is good
     if (!review) {
       const data = {
         isGood: false,
@@ -20,10 +28,20 @@ exports.addReview = async (req, res) => {
       return res.status(400).send(data);
     }
 
+    //check to see if req.response is a thing or not
+    if (!("response" in req) || req.response === undefined) req.response = {};
+
+    //attach review id to sauce if exists
+    if (!("sauce" in req.response) || req.response.sauce === undefined) {
+      req.response.sauce.review = review.toObject();
+    }
+
+    //attach review to response object
+    req.response.review = review.toObject();
     const data = {
       isGood: true,
       msg: "Successfully added sauce.",
-      data: req.body.return
+      data: req.response
     };
     return res.status(200).send(data);
   } catch (err) {
@@ -75,7 +93,6 @@ exports.findReviewByUserAndSauce = async (req, res) => {
  *  @param Integer Expects sauce._id on req.body
  *  @return array of reviews
  */
-
 exports.findReviewsBySauceID = async (req, res) => {
   //make sure sauce._id was actually passed
   if (!req.body.sauce || !req.body.sauce._id) {
