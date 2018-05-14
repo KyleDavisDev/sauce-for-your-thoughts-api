18e3efb424cc5d598635700cbe915db4acac8023
Kyle Bonar <kylewb2011@gmail.com>
2018-05-13 21:35:07 -0500

    Added Pepper model.

--- start.js
+++ start.js
@@ -1,21 +1,22 @@
 const mongoose = require("mongoose");
 
-//environment variables for variables.env file
+// environment variables for variables.env file
 require("dotenv").config({ path: "variables.env" });
 
-//connect to Database and handle any bad connections
+// connect to Database and handle any bad connections
 mongoose.connect(process.env.DATABASE);
-mongoose.Promise = global.Promise; //use ES6 promises
+mongoose.Promise = global.Promise; // use ES6 promises
 mongoose.connection.on("error", error => {
   console.log(error.message);
 });
 
-//import models --only need to do once per model
+// import models --only need to do once per model
 require("./models/Sauce.js");
 require("./models/User.js");
 require("./models/Review.js");
+require("./models/Pepper.js");
 
-//Start the app
+// Start the app
 const app = require("./app.js");
 app.set("port", process.env.PORT || 8080);
 const server = app.listen(app.get("port"), () => {
