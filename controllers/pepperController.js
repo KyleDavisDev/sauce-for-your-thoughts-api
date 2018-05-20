1db5e15eed20e2d0fbfa634928f3d680dee1adb9
Kyle Bonar <kylewb2011@gmail.com>
2018-05-20 16:38:43 -0500

    Method to grab peppers, assign to obj and go to next

--- controllers/pepperController.js
+++ controllers/pepperController.js
@@ -1,2 +1,34 @@
 const Pepper = require("mongoose").model("Pepper");
-const promisify = require("es6-promisify");
+
+/** @description Method to find all possible peppers
+ *  @extends req.response - semi 'global' object between middleware
+ */
+exports.getPeppers = async (req, res, next) => {
+  try {
+    const peppers = await Pepper.find({});
+
+    if (!peppers || peppers.length === 0) {
+      const data = {
+        isGood: false,
+        msg: "Could not find any peppers."
+      };
+      res.status(400).send(data);
+    }
+
+    // Init req.response if need be
+    if (req.response === undefined) req.response = {};
+
+    // Add peppers to req.response
+    req.response.peppers = peppers.map(x => x.toObject());
+
+    next();
+  } catch (err) {
+    const data = {
+      isGood: false,
+      msg:
+        "Something broke and we were unable to find any peppers. Please try again.",
+      err
+    };
+    return res.status(401).send(data);
+  }
+};
