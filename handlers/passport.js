2c22d23def2ca10dd36fe2fe35e3701b96192dfc
Kyle Bonar <kylewb2011@gmail.com>
2019-01-09 14:59:10 -0600

    Updated strategy as per documentation

--- handlers/passport.js
+++ handlers/passport.js
@@ -1,8 +1,9 @@
 const passport = require("passport");
+var LocalStrategy = require('passport-local').Strategy;
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 
-passport.use(User.createStrategy());
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
