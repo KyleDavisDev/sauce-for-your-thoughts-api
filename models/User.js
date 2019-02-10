aa082e8e7551fd2a997f219fa950d1cc23580e1e
Kyle Bonar <kylewb2011@gmail.com>
2019-02-10 16:59:25 -0600

    Additonal fields, added constants, isLocked virtual, increase loggin attempts method

--- models/User.js
+++ models/User.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+
 mongoose.Promise = global.Promise; // surpresses error (?)
-const validator = require("validator");
 const mongodbErrorHandler = require("mongoose-mongodb-errors");
 const passportLocalMongoose = require("passport-local-mongoose"); // takes care of salting pw
+
+const MAX_LOGIN_ATTEMPTS = 5;
+const LOCK_TIME = 20000;
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -14,7 +17,7 @@ const userSchema = new mongoose.Schema({
   },
   name: {
     type: String,
-    required: 'Please supply a name',
+    required: "Please supply a name",
     trim: true,
     unique: true
   },
@@ -23,10 +26,12 @@ const userSchema = new mongoose.Schema({
     default: Date.now
   },
   resetPasswordToken: String,
-  resetPasswordExpires: Date
+  resetPasswordExpires: Date,
+  loginAttempts: { type: Number, default: 0 },
+  lockedUntil: Date
 });
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function(next) {
   if (!this.isModified("name")) {
     return next(); // stop rest of function from running
   }
@@ -35,13 +40,21 @@ userSchema.pre("save", async function (next) {
     // Make sure email is unique
     let user = await this.constructor.find({ email: this.email }).limit(1);
     if (user.length > 0) {
-      return next(new Error("Oops! That email is already in use. If this is you, go to the log in page to log in.")); // throw error
+      return next(
+        new Error(
+          "Oops! That email is already in use. If this is you, go to the log in page to log in."
+        )
+      ); // throw error
     }
 
     // Make sure name is unique
     user = await this.constructor.find({ name: this.name }).limit(1);
     if (user.length > 0) {
-      return next(new Error("Oops! Someone is already using that name. Please try a different one.")); // throw error
+      return next(
+        new Error(
+          "Oops! Someone is already using that name. Please try a different one."
+        )
+      ); // throw error
     }
 
     // Keep on chuggin!
@@ -63,7 +76,45 @@ userSchema.set("toObject", {
   }
 });
 
-userSchema.plugin(passportLocalMongoose, { usernameField: "email" });
+// expose enum on the model
+userSchema.statics.failedLogin = {
+  NOT_FOUND: 0,
+  PASSWORD_INCORRECT: 1,
+  MAX_ATTEMPTS: 2
+};
+
+// Check if user is locked
+userSchema.virtual("isLocked").get(function() {
+  // check for a future lockUntil timestamp
+  return !!(this.lockUntil && this.lockUntil > Date.now());
+});
+
+userSchema.methods.incLoginAttempts = function(cb) {
+  // if we have a previous lock that has expired,
+  // reset loginAttempts counter and lockedUntil timer
+  if (this.lockedUntil && this.lockedUntil < Date.now()) {
+    return this.update(
+      {
+        $set: { loginAttempts: 1 },
+        $unset: { lockedUntil: 1 }
+      },
+      cb
+    );
+  }
+
+  // otherwise we're incrementing
+  const updates = { $inc: { loginAttempts: 1 } };
+  // lock the account if we've reached max attempts and it's not locked already
+  if (this.loginAttempts + 1 >= MAX_LOGIN_ATTEMPTS && !this.isLocked) {
+    updates.$set = { lockUntil: Date.now() + LOCK_TIME };
+  }
+  return this.update(updates, cb);
+};
+
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: "email",
+  attemptsField: "loginAttempts"
+});
 userSchema.plugin(mongodbErrorHandler); // change ugly errors to nice
 
 module.exports = mongoose.model("User", userSchema);
