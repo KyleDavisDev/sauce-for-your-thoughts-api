fb76a9d0834c51e578dc25e9d55b67fc2f07e1b4
Kyle Bonar <kylewb2011@gmail.com>
2019-02-10 23:30:10 -0600

    Manually handling password hashing

--- handlers/passport.js
+++ handlers/passport.js
@@ -1,9 +1,9 @@
-const passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
-const mongoose = require("mongoose");
-const User = mongoose.model("User");
+// const passport = require("passport");
+// var LocalStrategy = require("passport-local").Strategy;
+// const mongoose = require("mongoose");
+// const User = mongoose.model("User");
 
-passport.use(new LocalStrategy(User.authenticate()));
+// passport.use(new LocalStrategy(User.authenticate()));
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
