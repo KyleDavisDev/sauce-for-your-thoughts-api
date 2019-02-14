1ca2530d38536181a1b3af66a1651b7fcbb7436e
Kyle Bonar <kylewb2011@gmail.com>
2019-02-13 22:59:08 -0600

    removed unused code, using current collection funnctions, updated comments

--- models/User.js
+++ models/User.js
@@ -3,17 +3,9 @@ const bcrypt = require("bcrypt");
 
 mongoose.Promise = global.Promise; // surpresses error (?)
 const mongodbErrorHandler = require("mongoose-mongodb-errors");
-const passportLocalMongoose = require("passport-local-mongoose"); // takes care of salting pw
-
 const MAX_LOGIN_ATTEMPTS = 5;
 const LOCK_TIME = 20000;
 const SALT_WORK_FACTOR = 10;
-
-const reasons = {
-  NOT_FOUND: 0,
-  PASSWORD_INCORRECT: 1,
-  MAX_ATTEMPTS: 2
-};
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -37,7 +29,6 @@ const userSchema = new mongoose.Schema({
   },
   resetPasswordToken: String,
   resetPasswordExpires: Date,
-  logins: { type: Number, default: 0 },
   loginAttempts: { type: Number, default: 0 },
   lockedUntil: Date
 });
@@ -96,7 +87,7 @@ userSchema.methods.incLoginAttempts = function(cb) {
   // if we have a previous lock that has expired,
   // reset loginAttempts counter and lockedUntil timer
   if (this.lockedUntil && this.lockedUntil < Date.now()) {
-    return this.update(
+    return this.updateOne(
       {
         $set: { loginAttempts: 1 },
         $unset: { lockedUntil: 1 }
@@ -105,13 +96,14 @@ userSchema.methods.incLoginAttempts = function(cb) {
     );
   }
 
-  // otherwise we're incrementing
+  // otherwise we need to increase loginAttempts by 1
   const updates = { $inc: { loginAttempts: 1 } };
+
   // lock the account if we've reached max attempts and it's not locked already
   if (this.loginAttempts + 1 >= MAX_LOGIN_ATTEMPTS && !this.isLocked) {
-    updates.$set = { lockUntil: Date.now() + LOCK_TIME };
+    updates.$set = { lockedUntil: Date.now() + LOCK_TIME };
   }
-  return this.update(updates, cb);
+  return this.updateOne(updates, cb);
 };
 
 userSchema.statics.testAuthenticate = function(username, password, cb) {
@@ -122,32 +114,32 @@ userSchema.statics.testAuthenticate = function(username, password, cb) {
 
     // If cannot find user
     if (!user) {
-      return cb(null, null, reasons.NOT_FOUND);
+      return cb(null, null);
     }
 
     // See if account is locked
+    // We will skip hashing password and whatnot if it's locked
     if (user.isLocked) {
-      // just increment login attempts if account is already locked
+      // increment login attempts
       return user.incLoginAttempts(function(err) {
         if (err) return cb(err);
-        return cb(null, null, reasons.MAX_ATTEMPTS);
+        return cb(null, null);
       });
     }
 
-    // test for a matching password
-    user.comparePassword(password, function(err, isMatch) {
+    bcrypt.compare(password, user.password, function(err, isMatch) {
       if (err) return cb(err);
 
-      // check if the password was a match
       if (isMatch) {
         // if there's no lock or failed attempts, just return the user
         if (!user.loginAttempts && !user.lockUntil) return cb(null, user);
+
         // reset attempts and lock info
         var updates = {
           $set: { loginAttempts: 0 },
           $unset: { lockUntil: 1 }
         };
-        return user.update(updates, function(err) {
+        return user.updateOne(updates, function(err) {
           if (err) return cb(err);
           return cb(null, user);
         });
@@ -156,7 +148,7 @@ userSchema.statics.testAuthenticate = function(username, password, cb) {
       // password is incorrect, so increment login attempts before responding
       user.incLoginAttempts(function(err) {
         if (err) return cb(err);
-        return cb(null, null, reasons.PASSWORD_INCORRECT);
+        return cb(null, null);
       });
     });
   });
@@ -178,17 +170,6 @@ userSchema.methods.setPassword = async function(password) {
   }
 };
 
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};
-
-// userSchema.plugin(passportLocalMongoose, {
-//   usernameField: "email",
-//   attemptsField: "loginAttempts"
-// });
 userSchema.plugin(mongodbErrorHandler); // change ugly errors to nice
 
 module.exports = mongoose.model("User", userSchema);
