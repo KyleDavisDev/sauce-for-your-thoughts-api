379d32647004ab21b865d3037b0ab4b47b472a04
Kyle Bonar <kylewb2011@gmail.com>
2019-02-19 00:01:27 -0600

    removed console logs

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,7 @@ exports.login = (req, res) => {
     return res.status(400).send(data);
   }
 
-  User.testAuthenticate(req.body.user.email, req.body.user.password, function (
+  User.testAuthenticate(req.body.user.email, req.body.user.password, function(
     err,
     result,
     msg
@@ -56,7 +56,6 @@ exports.isLoggedIn = (req, res, next) => {
     ) {
       // convert string to object
       const obj = JSON.parse(req.body.user);
-      console.log(obj);
 
       // concat onto req.body
       Object.keys(obj).forEach(x => {
@@ -72,10 +71,8 @@ exports.isLoggedIn = (req, res, next) => {
     }
   }
 
-  console.log(req.body);
   // get token from post
   const { token } = req.body.user;
-  console.log(token);
 
   // decode the token using a secret key-phrase
   return jwt.verify(token, process.env.SECRET, (err, decoded) => {
@@ -136,7 +133,7 @@ exports.forgot = async (req, res) => {
     // create URL and email to user email
     const resetURL = `http://localhost:8080/account/reset/${
       user.resetPasswordToken
-      }`;
+    }`;
     await mail.send({
       user,
       subject: "Password reset",
