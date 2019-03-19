090dbf2dabfd20a6768f045f61569112b3745fbe
Kyle Bonar <kylewb2011@gmail.com>
2019-03-18 22:07:15 -0500

    added insert method

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,12 @@
-export const UserTable = `CREATE TABLE IF NOT EXISTS Users (
+var db = require("../db.js");
+const bcrypt = require("bcrypt");
+
+// Constants
+const MAX_LOGIN_ATTEMPTS = 5;
+const LOCK_TIME = 1000 * 60 * 60 * 2; // 2 hour lock
+const SALT_WORK_FACTOR = 10;
+
+export const UserTableStructure = `CREATE TABLE IF NOT EXISTS Users (
   UserID int NOT NULL AUTO_INCREMENT,
   Email varchar(50) NOT NULL UNIQUE,
   Password varchar(100) NOT NULL,
@@ -10,3 +18,19 @@ export const UserTable = `CREATE TABLE IF NOT EXISTS Users (
   LockedUntil DATETIME,
   PRIMARY KEY (UserID)
   );`;
+
+export const UserTableRemove = "DROP TABLE Users";
+
+exports.insert = async function({ email, password, displayName }, cb) {
+  // Create salt
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+  // Create hash
+  const hash = await bcrypt.hash(password, salt);
+
+  const values = { Email: email, Password: hash, DisplayName: displayName };
+
+  db.get().query("INSERT INTO Users SET ?", values, function(err, result) {
+    if (err) return cb(err);
+    cb(null, result.insertId);
+  });
+};
