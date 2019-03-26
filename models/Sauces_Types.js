0e973b866ee47ceb1ad93a94214c7dbe96393f98
Kyle Bonar <kylewb2011@gmail.com>
2019-03-26 13:21:43 -0500

    init insert method, import db

--- models/Sauces_Types.js
+++ models/Sauces_Types.js
@@ -1,3 +1,5 @@
+var db = require("../db/db.js");
+
 exports.SaucesTypesTableStructure = `CREATE TABLE IF NOT EXISTS Sauces_Types (
   SauceID int,
   TypeID int,
@@ -5,3 +7,7 @@ exports.SaucesTypesTableStructure = `CREATE TABLE IF NOT EXISTS Sauces_Types (
   FOREIGN KEY (SauceID) REFERENCES Sauces(SauceID),
   FOREIGN KEY (TypeID) REFERENCES Types(TypeID)
   );`;
+
+// exports.Insert = function({ Types }, cb) {
+//   db.get().query("")
+// };
