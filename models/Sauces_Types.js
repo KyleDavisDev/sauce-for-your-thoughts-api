a247dfeb348a6a8a332c04d5dbfb04e08e16e419
Kyle Bonar <kylewb2011@gmail.com>
2019-03-27 15:33:47 -0500

    Explicity define table

--- models/Sauces_Types.js
+++ models/Sauces_Types.js
@@ -6,7 +6,7 @@ exports.SaucesTypesTableStructure = `CREATE TABLE IF NOT EXISTS Sauces_Types (
   PRIMARY KEY (SauceID, TypeID),
   FOREIGN KEY (SauceID) REFERENCES Sauces(SauceID),
   FOREIGN KEY (TypeID) REFERENCES Types(TypeID)
-  );`;
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;`;
 
 exports.Insert = async function({ SauceID, TypeID, record }) {
   // If full record is sent, use this
