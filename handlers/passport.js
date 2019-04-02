4bab45beb59f94c682663ac11d248c4cae1adc6a
Kyle Bonar <kylewb2011@gmail.com>
2019-04-02 09:15:09 -0500

    Removed unused code

--- handlers/passport.js
+++
@@ -1,9 +0,0 @@
-// const passport = require("passport");
-// var LocalStrategy = require("passport-local").Strategy;
-// const mongoose = require("mongoose");
-// const User = mongoose.model("User");
-
-// passport.use(new LocalStrategy(User.authenticate()));
-
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
