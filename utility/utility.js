2e1725fffa95c77c13737ad8c161fa59d58f3330
Kyle Bonar <kylewb2011@gmail.com>
2019-04-29 14:48:05 -0500

    Method to see if middleware is last in or not.

--- utility/utility.js
+++ utility/utility.js
@@ -1,5 +1,23 @@
 class utility {
   constructor() {}
+
+  /** @description Get all reviews related to specific sauce slug.
+   *  @param {String} name - Name of current middleware
+   *  @param {Layer[]} stack - Array of middleware layers
+   *  @return Attaches reviews to sauce.
+   */
+  isLastMiddlewareInStack({ name, stack }) {
+    let position = 0;
+    for (let i = 0, len = stack.length; i < len; i++) {
+      const middleware = stack[i];
+      if (middleware.name === name) {
+        position = i;
+        break;
+      }
+    }
+
+    return position + 1 == stack.length;
+  }
 }
 
 const Utility = new utility();
