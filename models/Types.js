71ba1b876bf9c3cf767885e117b6acfdacc14f46
Kyle Bonar <kylewb2011@gmail.com>
2019-05-09 11:19:36 -0500

    Method to get active types, returns array

--- models/Types.js
+++ models/Types.js
@@ -9,9 +9,21 @@ exports.TypesTableStructure = `CREATE TABLE IF NOT EXISTS Types (
 
 // Returns array of TypeIDs
 exports.FindIDByValues = async function({ Values }) {
-  const rows = await DB.query("SELECT TypeID FROM Types WHERE Value IN ?", [
-    [Values]
-  ]);
+  const rows = await DB.query(
+    "SELECT TypeID FROM Types WHERE Value IN ? AND IsActive=1",
+    [[Values]]
+  );
 
-  return rows.map(row => row.TypeID);
+  return rows.map(row => {
+    return row.TypeID;
+  });
+};
+
+// Returns array of active types
+exports.FindTypes = async function() {
+  const rows = await DB.query("SELECT Value FROM Types WHERE IsActive = 1");
+
+  return rows.map(row => {
+    return row.Value;
+  });
 };
