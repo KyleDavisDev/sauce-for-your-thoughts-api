5a04da1f79c3045f3933de9cdcbab0bfe7204903
Kyle Bonar <kylewb2011@gmail.com>
2019-06-12 10:40:42 -0500

    Switching over to use cloudinary primariliy

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,7 +1,16 @@
+require("dotenv").config({ path: "variables.env" });
 const multer = require("multer"); // helps uploading images/files
 const jimp = require("jimp"); // helps with resizing photos
 const uuid = require("uuid"); // generated unique identifiers
 var fs = require("fs"); // file system
+var cloudinary = require("cloudinary").v2;
+const Datauri = require("datauri");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+});
 
 const multerOptions = {
   storage: multer.memoryStorage(),
@@ -12,13 +21,37 @@ const multerOptions = {
     } else {
       next({ message: "That filetype is not allowed" }, false);
     }
-  },
-  dest: "uploads/"
+  }
+  // dest: "uploads/"
 };
 
 exports.upload = multer(multerOptions).single("image");
 
-exports.resize = async (req, res, next) => {
+// exports.resize = async (req, res, next) => {
+//   // check if new file to resize
+//   if (!req.file) {
+//     next(); // go to next middleware
+//     return;
+//   }
+//   // get file extension and generate unique name
+//   const extension = req.file.mimetype.split("/")[1];
+//   req.body.photo = `${uuid.v4()}.${extension}`;
+
+//   // resize photo
+//   try {
+//     const photo = await jimp.read(req.file.buffer);
+//     await photo.resize(800, jimp.AUTO);
+//     const dir = "./public/uploads";
+//     await photo.write(`${dir}/${req.body.photo}`); // Save point
+//     req.body.sauce.photo = req.file;
+//     next();
+//   } catch (err) {
+//     console.log(err);
+//     next({ message: "Image was unable to be saved" }, false);
+//   }
+// };
+
+exports.saveImage = async (req, res, next) => {
   // check if new file to resize
   if (!req.file) {
     next(); // go to next middleware
@@ -28,16 +61,21 @@ exports.resize = async (req, res, next) => {
   const extension = req.file.mimetype.split("/")[1];
   req.body.photo = `${uuid.v4()}.${extension}`;
 
-  // resize photo
+  // upload photo
   try {
-    const photo = await jimp.read(req.file.buffer);
-    await photo.resize(800, jimp.AUTO);
-    const dir = "./public/uploads";
-    await photo.write(`${dir}/${req.body.photo}`); // Save point
-    req.body.sauce.photo = req.file;
-    next();
+    // Convert buffer into something cloudinary can handle
+    var dUri = new Datauri();
+    dUri.format(".png", req.file.buffer);
+
+    // Upload image
+    cloudinary.uploader.upload(dUri.content, function(err, img) {
+      console.log("ERR: ", err);
+      console.log("IMG: ", img);
+    });
+    // console.log(test);
   } catch (err) {
     console.log(err);
-    next({ message: "Image was unable to be saved" }, false);
   }
+
+  // return next();
 };
