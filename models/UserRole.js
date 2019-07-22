1e727e6e5e3cf6404da495c254449c5dbc594046
Kyle Bonar <kylewb2011@gmail.com>
2019-07-21 21:59:10 -0500

    Update to include user id too

--- models/UserRole.js
+++ models/UserRole.js
@@ -1,7 +1,10 @@
 exports.UserRoleTableStructure = `CREATE TABLE 'UserRole' (
   'UserRoleID' int(11) unsigned NOT NULL AUTO_INCREMENT,
   'RoleID' int(11) NOT NULL,
+  'UserID' int(11) NOT NULL,
   PRIMARY KEY ('UserRoleID'),
   KEY 'UserRole_RoleID_Roles_RoleID' ('RoleID'),
-  CONSTRAINT 'UserRole_RoleID_Roles_RoleID' FOREIGN KEY ('RoleID') REFERENCES 'Roles' ('RoleID')
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;`;
+  KEY 'UserRole_UserID_Users_UserID' ('UserID'),
+  CONSTRAINT 'UserRole_RoleID_Roles_RoleID' FOREIGN KEY ('RoleID') REFERENCES 'Roles' ('RoleID'),
+  CONSTRAINT 'UserRole_UserID_Users_UserID' FOREIGN KEY ('UserID') REFERENCES 'Users' ('UserID')
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;`;
