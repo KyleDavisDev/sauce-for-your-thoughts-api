4e87437ad090903ce8431c8e6a5777cbf8c32f9c
Kyle Bonar <kylewb2011@gmail.com>
2019-08-05 09:31:23 -0500

    Updated table structure, updated comments, return only array of strings

--- models/Avatars.js
+++ models/Avatars.js
@@ -2,18 +2,34 @@ const DB = require("../db/db.js");
 
 exports.ReviewsTableStructure = `CREATE TABLE 'Avatars' (
   'AvatarID' int(11) unsigned NOT NULL AUTO_INCREMENT,
-  'Name' tinytext CHARACTER SET utf8 NOT NULL,
+  'URL' tinytext CHARACTER SET utf8 NOT NULL,
   'IsActive' tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY ('AvatarID')
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;`;
 
+/** @description Get all Avatar URLs from DB
+ *  @return {Promise}
+ *  @resolves {string[]} array of strings
+ *  @reject {String} error message
+ */
 exports.getAll = async () => {
   const res = await DB.query(`
   SELECT
-    Name
+    URL
   FROM
     Avatars
   WHERE
     IsActive = 1`);
-  console.log(res);
+
+  // Make sure could find avatars
+  if (!res) {
+    throw new Error("Error retrieving avatars. Please try again.");
+  }
+
+  const urls = [];
+  for (let i = 0, len = res.length; i < len; i++) {
+    urls.push(res[i].URL);
+  }
+
+  return urls;
 };
