dc993ceb604f817c97b2071af55e079dca976bf2
Kyle Bonar <kylewb2011@gmail.com>
2019-08-21 15:44:27 -0500

    New route for checking if user can submit a sauce or not

--- routes/routes.js
+++ routes/routes.js
@@ -30,6 +30,14 @@ router.post(
   sauceController.addSauce
 );
 
+// 1. Check if user.token is legit, place user's _id onto user
+// 2. Check if user can submit a sauce or not
+router.post(
+  "/api/review/canusersubmit",
+  authController.isLoggedIn,
+  sauceController.canUserSubmit
+);
+
 // 1. Make sure slug param is legit
 // 2. Find sauce by unique slug
 // 3. Find reviews related to sauce
