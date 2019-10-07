9792c3032b8b1be96da2cb08a015c5ae8106e0be
Kyle Bonar <kylewb2011@gmail.com>
2019-10-07 10:31:35 -0500

    Remove unused code

--- start.js
+++ start.js
@@ -1,28 +1,5 @@
-// const mongoose = require("mongoose");
-
 // environment variables for variables.env file
 require("dotenv").config({ path: "variables.env" });
-
-// connect to Database and handle any bad connections
-// mongoose.connect(process.env.DATABASE, {
-//   useNewUrlParser: true,
-//   useCreateIndex: true
-// });
-// mongoose.Promise = global.Promise; // use ES6 promises
-// mongoose.connection.on("error", error => {
-//   console.log(error.message);
-// });
-
-// Surpress error
-// mongoose.set("useCreateIndex", true);
-
-// import models --only need to do once per model
-// require("./models/Sauce.js");
-// require("./models/User.js");
-// require("./models/Review.js");
-// require("./models/Pepper.js");
-// require("./models/Type.js");
-// const Users = require("./models/Users");
 
 var DB = require("./db/db.js");
 
