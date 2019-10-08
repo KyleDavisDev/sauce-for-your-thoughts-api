7d1736d019431b07d7bbd09e52d714b34e7da05c
Kyle Bonar <kylewb2011@gmail.com>
2019-10-08 10:19:52 -0500

    Insert method

--- models/BigBrother.js
+++ models/BigBrother.js
@@ -13,3 +13,52 @@ exports.ReviewsTableStructure = `CREATE TABLE BigBrother (
   CONSTRAINT BigBrother_SauceID_Sauces_SauceID FOREIGN KEY (BigBrotherID) REFERENCES Sauces (SauceID),
   CONSTRAINT BigBrother_UserID_Users_UserID FOREIGN KEY (BigBrotherID) REFERENCES Users (UserID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;`;
+
+/** @description Insert record into database.
+ *  @param {Number?} UserID - user id
+ *  @param {Number?} SauceID - sauce id
+ *  @param {Number?} ReviewID - review id
+ *  @param {String} Action - what is happening
+ *  @param {Number?} Duration - How long it is taking (in ms)
+ *  @param {String} IP - IP used to make request
+ *  @return inserted result
+ */
+exports.Insert = async function({
+  UserID,
+  SauceID,
+  ReviewID,
+  Action,
+  Duration,
+  IP
+}) {
+  // Throw error if not all info is available.
+  if (!Action || !IP) {
+    throw new Error(
+      "Could insert action into database. Please make sure all required information is provided."
+    );
+  }
+
+  const values = {
+    UserID,
+    SauceID,
+    ReviewID,
+    Action,
+    Duration,
+    IP
+  };
+
+  const rows = DB.query(
+    `
+    INSERT INTO
+      BigBrother
+    SET ?`,
+    values
+  );
+
+  // Make sure we could save user
+  if (!rows) {
+    throw new Error("Error trying to save user. Please try again.");
+  }
+
+  return rows;
+};
