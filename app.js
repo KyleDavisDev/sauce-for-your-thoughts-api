fc434f3dc535e9285862812558a2f2ea2a0fc687
Kyle Bonar <kylewb2011@gmail.com>
2019-10-09 09:52:24 -0500

    Import middleware functions from respective files.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const routes = require("./routes/routes.js");
-const requestIp = require("request-ip");
 var useragent = require("express-useragent");
-const BigBrother = require("./models/BigBrother");
+
+const BigBrotherMiddleware = require("./middleware/BigBrotherMiddleware");
+const CrossOriginMiddleware = require("./middleware/CrossOriginMiddleware");
 
 //create express app
 const app = express();
@@ -10,46 +11,15 @@ const app = express();
 // get user info. Attaches to req.useragent
 app.use(useragent.express());
 
+// Allow cross origin
+app.use(CrossOriginMiddleware);
+
+// Start recording what person is doing
+app.use(BigBrotherMiddleware);
+
 //serves up static files from distribution and images folder.
 app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 app.use("/public/avatars", express.static(__dirname + "/public/avatars"));
-
-// Allow cross origin
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-// Middleware for BigBrother to see what's going on.
-const BigBrotherMiddleware = async function(req, res, next) {
-  try {
-    // Don't bother if just getting options
-    if (req.method !== "OPTIONS") {
-      const clientIp = requestIp.getClientIp(req);
-      const BigBrotherID = await BigBrother.Insert({
-        IP: clientIp,
-        Action: "initiate request"
-      });
-
-      // Save ID to locals for later
-      res.locals.BigBrotherID = BigBrotherID;
-    }
-  } catch (err) {
-    console.log(err);
-  }
-
-  // res.on("finish", function() {
-  //   console.log("after");
-  // });
-
-  next();
-};
-
-app.use(BigBrotherMiddleware);
 
 // takes raw requests and attaches them to req.body for use later
 app.use(express.json());
