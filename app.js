46b89f014e0e43fba3dea47793e2a9ea9b7493ea
Kyle Bonar <kylewb2011@gmail.com>
2019-10-09 09:44:25 -0500

    Attach id to res.locals, get user info

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const routes = require("./routes/routes.js");
 const requestIp = require("request-ip");
+var useragent = require("express-useragent");
 const BigBrother = require("./models/BigBrother");
 
 //create express app
 const app = express();
+
+// get user info. Attaches to req.useragent
+app.use(useragent.express());
 
 //serves up static files from distribution and images folder.
 app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
@@ -20,13 +24,19 @@ app.use(function(req, res, next) {
   next();
 });
 
-// inside middleware handler
-const ipMiddleware = async function(req, res, next) {
+// Middleware for BigBrother to see what's going on.
+const BigBrotherMiddleware = async function(req, res, next) {
   try {
     // Don't bother if just getting options
     if (req.method !== "OPTIONS") {
       const clientIp = requestIp.getClientIp(req);
-      const rows = await BigBrother.Insert({ IP: clientIp, Action: "test" });
+      const BigBrotherID = await BigBrother.Insert({
+        IP: clientIp,
+        Action: "initiate request"
+      });
+
+      // Save ID to locals for later
+      res.locals.BigBrotherID = BigBrotherID;
     }
   } catch (err) {
     console.log(err);
@@ -39,7 +49,7 @@ const ipMiddleware = async function(req, res, next) {
   next();
 };
 
-app.use(ipMiddleware);
+app.use(BigBrotherMiddleware);
 
 // takes raw requests and attaches them to req.body for use later
 app.use(express.json());
