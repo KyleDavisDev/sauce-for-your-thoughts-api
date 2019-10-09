b0b5d7e6f7c545a0e1c2c0de38d9fd7c80d28fa1
Kyle Bonar <kylewb2011@gmail.com>
2019-10-09 09:28:57 -0500

    Updated table structure, fixed typo, added default vals on insert

--- models/BigBrother.js
+++ models/BigBrother.js
@@ -2,16 +2,16 @@ const DB = require("../db/db.js");
 
 exports.ReviewsTableStructure = `CREATE TABLE BigBrother (
   BigBrotherID int(11) NOT NULL AUTO_INCREMENT,
-  UserID int(11) DEFAULT NULL,
-  SauceID int(11) DEFAULT NULL,
-  ReviewID int(11) NOT NULL,
+  UserID int(11) NULL,
+  SauceID int(11) NULL,
+  ReviewID int(11) NULL,
   Action varchar(200) NOT NULL DEFAULT '',
   Duration int(11) DEFAULT NULL,
   IP varchar(30) NOT NULL DEFAULT '',
   PRIMARY KEY (BigBrotherID),
-  CONSTRAINT BigBrother_ReviewID_Reviews_ReviewID FOREIGN KEY (BigBrotherID) REFERENCES Reviews (ReviewID),
-  CONSTRAINT BigBrother_SauceID_Sauces_SauceID FOREIGN KEY (BigBrotherID) REFERENCES Sauces (SauceID),
-  CONSTRAINT BigBrother_UserID_Users_UserID FOREIGN KEY (BigBrotherID) REFERENCES Users (UserID)
+  CONSTRAINT BigBrother_ReviewID_Reviews_ReviewID FOREIGN KEY (ReviewID) REFERENCES Reviews (ReviewID),
+  CONSTRAINT BigBrother_SauceID_Sauces_SauceID FOREIGN KEY (SauceID) REFERENCES Sauces (SauceID),
+  CONSTRAINT BigBrother_UserID_Users_UserID FOREIGN KEY (UserID) REFERENCES Users (UserID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;`;
 
 /** @description Insert record into database.
@@ -24,11 +24,11 @@ exports.ReviewsTableStructure = `CREATE TABLE BigBrother (
  *  @return inserted result
  */
 exports.Insert = async function({
-  UserID,
-  SauceID,
-  ReviewID,
+  UserID = null,
+  SauceID = null,
+  ReviewID = null,
   Action,
-  Duration,
+  Duration = null,
   IP
 }) {
   // Throw error if not all info is available.
@@ -37,22 +37,27 @@ exports.Insert = async function({
       "Could insert action into database. Please make sure all required information is provided."
     );
   }
+  // const query = `
+  // INSERT INTO
+  //     BigBrother
+  //   SET
+  //     Action = ${Action},
+  //     IP = ${IP},
 
-  const values = {
-    UserID,
-    SauceID,
-    ReviewID,
-    Action,
-    Duration,
-    IP
-  };
+  //     `
 
   const rows = DB.query(
     `
     INSERT INTO
       BigBrother
-    SET ?`,
-    values
+    SET 
+      Action = ?,
+      IP = ?, 
+      UserID = ?,
+      SauceID = ?,
+      ReviewID = ?;  
+      `,
+    [Action, IP, UserID, SauceID, ReviewID]
   );
 
   // Make sure we could save user
