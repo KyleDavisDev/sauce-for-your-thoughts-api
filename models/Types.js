8baeec2bb1cf421741d2981f1f7f6bdbec06150d
Kyle Bonar <kylewb2011@gmail.com>
2019-10-29 06:42:15 -0500

    Update syntax error

--- models/Types.js
+++ models/Types.js
@@ -3,7 +3,7 @@ var DB = require("../db/db.js");
 exports.TypesTableStructure = `CREATE TABLE IF NOT EXISTS Types (
   TypeID int NOT NULL AUTO_INCREMENT,
   Value varchar(50) NOT NULL, 
-  IsActive BOOLEAN DEFAULT '1'
+  IsActive BOOLEAN DEFAULT '1',
   PRIMARY KEY (TypeID)
   );`;
 
