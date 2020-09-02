82f811f1294f25adc6841bdbd6adaef3f77f3c51
Kyle Bonar <kylewb2011@gmail.com>
2020-09-02 12:23:26 -0500

    Add acceptable CORS header

--- middleware/CrossOriginMiddleware.js
+++ middleware/CrossOriginMiddleware.js
@@ -1,6 +1,7 @@
 const CrossOriginMiddleware = function(req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://sauceforyourthoughts.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, Content-Type, Accept, Authorization, X-Request-With"
