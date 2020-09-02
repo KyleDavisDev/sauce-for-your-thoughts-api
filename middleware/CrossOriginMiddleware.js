0d2cb50ef8ef4854708fcddf372c804930dbeed9
Kyle Bonar <kylewb2011@gmail.com>
2020-09-02 12:28:29 -0500

    Can only return single Access Control header so need to check first

--- middleware/CrossOriginMiddleware.js
+++ middleware/CrossOriginMiddleware.js
@@ -1,7 +1,13 @@
 const CrossOriginMiddleware = function(req, res, next) {
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://sauceforyourthoughts.com"
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Origin", "https://sauceforyourthoughts.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, Content-Type, Accept, Authorization, X-Request-With"
